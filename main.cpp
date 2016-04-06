caf81f8292a3f0470569f5e7c6fb870c75500810
Timon Knigge <timonknigge@live.nl>
2016-04-06 14:34:50 +0200

    Layout of the matrix class.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "matrix.cpp"
+
+
+int main(int argc, char *argv[]) {
+	
+	if (argc > 1) {
+		std::cerr << "Usage:\n\t" << argv[0];
+		std::cerr << " < infile > outfile\n\n";
+		std::cerr << "\tinfile: Input matrix in Matrix Market format.\n";
+		std::cerr << "\toutfile: Output location." << std::endl;
+	} else {
+		std::cerr << argv[0] << std::endl;
+	}
+	
+	return 0;
+}
---
+++ matrix.h
@@ -0,0 +1,47 @@
+/*
+	matrix.h
+	
+	Representation of a matrix to be partitioned.
+*/
+
+#include <iostream>
+#include <vector>
+#include <memory>
+
+constexpr size_t ROW = 0, COL = 1;
+
+// Represents a non-zero in the matrix
+struct entry {
+	// The row and column in which the entry is positioned
+	size_t pos[2], &r = pos[ROW], &c = pos[COL];
+	// The indices in the row and column list of the matrix
+	size_t index[2], &ri = index[ROW], &ci = index[COL];
+};
+
+
+class matrix {
+
+public:
+	// The entries of the matrix, by row and column
+	std::vector< std::vector< std::shared_ptr<entry> > > 
+		adj[2], &rows = adj[0], &cols = adj[1];
+	
+	// The number of rows, columns and non-zeros in the matrix
+	size_t R, C;
+	int NZ = 0;
+
+private:
+	// Add a nonzero entry in position (r, c) with 0<=r<R and
+	// 0<=c<C
+	void add_entry(size_t r, size_t c);
+	
+	// Fixes the internal adjacency lists
+	void sort_adjacency_lists();
+	
+	// Overload I\O
+	friend std::istream &operator>>(std::istream &stream,       matrix &m);
+	friend std::ostream &operator<<(std::ostream &stream, const matrix &m);
+};
+
+
+
