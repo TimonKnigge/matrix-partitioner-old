b33d3518a8ace55d30ef69c7530ccc727ac381e9
Timon Knigge <timonknigge@live.nl>
2016-04-11 10:10:51 +0200

    Adds an abstract representation of the partitioner.

---
+++ abstract_partitioner.h
@@ -0,0 +1,18 @@
+/*
+	abstract_partitioner.h
+	
+	Describes the interface of a partitioner
+*/
+
+#pragma once
+
+#include <vector>
+
+#include "matrix.h"
+
+
+class abstract_partitioner {
+	
+	
+};
+
