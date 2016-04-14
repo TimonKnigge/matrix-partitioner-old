3c0cf59e82ae6fdfab0db7e1ec260251f8b8950d
Timon Knigge <timonknigge@live.nl>
2016-04-14 12:52:58 +0200

    Adds row/col statusses to the abstract partitioner.

--- abstract_partitioner.h
+++ abstract_partitioner.h
@@ -10,6 +10,17 @@
 
 #include "matrix.h"
 
+enum class status {
+	unassigned, red, blue, cut, 
+	partialblue, partialred, implicitcut
+};
+
+status to_partial(status st) {
+	return (st == status.red ? status.partialred : status.partialblue);
+}
+status from_partial(status st) {
+	return (st == status.partialred ? status.red : status.blue);
+}
 
 class abstract_partitioner {
 	
