913b9a6b7fbbdeaa54388024c08726ac7d713a92
Timon Knigge <timonknigge@live.nl>
2016-05-01 11:38:56 +0200

    Defines the abstract partitioner.

--- abstract_partitioner.h
+++ abstract_partitioner.h
@@ -23,7 +23,20 @@ status from_partial(status st) {
 }
 
 class abstract_partitioner {
+
+public:
+	// Constructs a matrix partitioner around the given matrix.
+	abstract_partitioner(matrix &_m)
+		: m(_m) { }
 	
+	// Partitions a matrix with an (1+epsilon) balance. Returns false 
+	// if the matrix could not be partitioned, and true otherwise. If
+	// the matrix is successfully partitioned, the row and col vectors
+	// contain an assignment of the rows and columns (all elements will
+	// have the form unassigned, red or blue).
+	virtual bool partition(double epsilon, vector<status> &row, vector<status> &col) = 0;
 	
+protected:
+	matrix &m
 };
 
