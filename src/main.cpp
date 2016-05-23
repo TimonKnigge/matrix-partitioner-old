2b398deaba24cbce8ce888378431a533e483ab13
Timon Knigge <timonknigge@live.nl>
2016-05-23 10:38:08 +0200

    Epsilon displayed now.

--- src/main.cpp
+++ src/main.cpp
@@ -22,14 +22,17 @@ int main(int argc, char *argv[]) {
 		
 		matrix m;
 		std::cin >> m;
+		double epsilon = 0.03;
+		
 		std::cerr << m.R << 'x' << m.C << " matrix has " << m.NZ << " nonzeros (Cmax = " << m.Cmax << ")." << std::endl;
 		std::cerr << m << std::flush;
 		
 		branchandbound bb(m);
 		std::vector<status> r, c;
-		std::cerr << "Volume: " << bb.partition(0.03, r, c) << ", ";
+		std::cerr << "Volume: " << bb.partition(epsilon, r, c) << ", ";
 		std::cerr << bb.leaves << " leaves visited." << std::endl;
 		print_colored(m, r, c);
+		std::cerr << "epsilon = " << epsilon << " allowed for a maximal partition size of " << bb.partial_partition.max_partition_size << std::endl;
 	}
 	
 	return 0;
